70103388bd37ccc90df17551dfcade37b3ad0dd2
Devendra Parihar (CW) <Devendra.Parihar@jda.com>
2022-02-25 10:07:37 +0530

    update

--- src/components/constants/APIURL.js
+++ src/components/constants/APIURL.js
@@ -5,5 +5,5 @@
 export const APIURL = "https://api.ayekart.com/v1";
 
 // export const APIURLNEW = "http://139.59.91.183/ayekart/api";
-export const APIURLNEW = "https://cors-everywhere.herokuapp.com";
+export const APIURLNEW = "https://cors-everywhere.herokuapp.com/http://139.59.91.183/ayekart/api";
 
